9dfa11ce35835bdd84faec933e0240734b0a88ef
Арег <areg@MacBook-Air-Areg.local>
2025-03-02 19:39:12 +0300

    previous version

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,8 @@ import { CreditsCard } from "./components/CreditsCard.js";
 import { CreditsSection } from "./components/CreditsSection.js";
 import { initialCards } from "./utils/constants.js";
 
+const logo = document.querySelector('.header__icon');
+
 function createCredit({price, amount}){
   const credit = new CreditsCard({price, amount}, '.order__template-card').createCredit();
   return credit;
@@ -15,14 +17,50 @@ const cardSectionInstance = new CreditsSection({renderer: ({amount, price}) => {
 
 console.log('hello');
 const swiper = new Swiper('.swiper', {
-  slidesPerView: 1,
   pagination:{
     el: '.swiper-pagination',
     clickable: true,
   },
   speed: 400,
+  slidesPerView: "auto",
+  width: 200,
   spaceBetween: 100,
-  width: 260,
+  breakpoints: {
+    768:{
+      slidesPerView: 1,
+      spaceBetween: 0,
+      width: 150
+    }
+  }
 });
 
-cardSectionInstance.rendererItems(initialCards)
+cardSectionInstance.rendererItems(initialCards);
+
+window.addEventListener('DOMContentLoaded', () => {
+  const profile = document.querySelector('.profile'),
+        hamburger = document.querySelector('.hamburger'),
+        header = document.querySelector('.header'),
+        title = document.querySelector('.order__title');
+
+  hamburger.addEventListener('click', () => {
+    header.classList.toggle('header_active');
+    hamburger.classList.toggle('hamburger_active');
+    profile.classList.toggle('profile_active');
+    title.classList.toggle('order__title_active');
+  });
+
+  if(window.innerWidth > 768){
+    logo.src = './images/logo.svg';
+  }else{
+    logo.src = './images/diamond.svg';
+  }
+
+})
+
+window.addEventListener('resize', ()=>{
+  if(window.innerWidth > 768){
+    logo.src = './images/logo.svg';
+  }else {
+    logo.src = './images/diamond.svg';
+  }
+})
